96ffdb3fbc58ee244aa88190c5433471d495d0cb
Jan Zarsky <xzarsk03@stud.fit.vutbr.cz>
2017-04-30 16:10:57 +0200

    implement parsing of move command

--- src/Command.hpp
+++ src/Command.hpp
@@ -6,18 +6,22 @@ namespace solitaire
     // final = 4 hromadky vpravo nahore
     // pile = 7 hromadek dole
     enum CommandType {
-        turn_deck, // otoc kartu z balicku
         move_deck_pile, // presun z balicku do pile
-        move_deck_final,
+        move_deck_home,
         move_pile_pile,
-        move_pile_final,
+        move_pile_home,
+        turn_deck, // otoc kartu z balicku
         turn_pile // otoc kartu z pile, ktera je byla otocena dolu
     };
 
     struct Command
     {
         CommandType type;
-        Command(CommandType t): type{t} {}
+        int from;
+        int to;
+        int count;
+
+        Command(CommandType t): type{t}, from{0}, to{0}, count{0} {}
         Command() {}
     };
 }
--- src/TextUIParser.cpp
+++ src/TextUIParser.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "TextUIParser.hpp"
 #include "Exceptions.hpp"
+#include "Game.hpp"
 
 using namespace std;
 
@@ -39,8 +40,89 @@ namespace solitaire
         // game commands
         else if (str == "move" || str == "m") {
             cmd.type = game_cmd;
-            cmd.game_cmd.type = move_deck_pile;
+
+            try {
+                getline(ss, str, ' ');
+
+                if (str == "deck") {
+                    getline(ss, str, ' ');
+
+                    if (str.substr(0, 4) == "pile") {
+                        // move deck pileX
+                        cmd.game_cmd.type = move_deck_pile;
+                        cmd.game_cmd.to = stoi(str.substr(4));
+
+                        if (cmd.game_cmd.to < 1 || cmd.game_cmd.to > NUM_OF_COLUMNS)
+                            cmd.type = invalid;
+                    }
+                    else if (str.substr(0, 4) == "home") {
+                        // move deck homeX
+                        cmd.game_cmd.type = move_deck_home;
+                        cmd.game_cmd.to = stoi(str.substr(4));
+
+                        if (cmd.game_cmd.to < 1 || cmd.game_cmd.to > NUM_OF_HOMES)
+                            cmd.type = invalid;
+                    }
+                    else {
+                        cmd.type = invalid;
+                    }
+                    
+                }
+                else if (str.substr(0, 4) == "pile") {
+                    getline(ss, str, ' ');
+                    
+                    cmd.game_cmd.from = stoi(str.substr(4));
+
+                    if (cmd.game_cmd.from < 1 || cmd.game_cmd.from > NUM_OF_COLUMNS)
+                        cmd.type = invalid;
+
+                    if (str.substr(0, 4) == "pile") {
+                        // move pileX pileX
+                        cmd.game_cmd.type = move_pile_pile;
+                        cmd.game_cmd.to = stoi(str.substr(4));
+
+                        if (cmd.game_cmd.to < 1 || cmd.game_cmd.to > NUM_OF_COLUMNS)
+                            cmd.type = invalid;
+
+                        if (ss.eof()) {
+                            cmd.game_cmd.count = 1;
+                        }
+                        else {
+                            getline(ss, str);
+
+                            if (str.empty()) {
+                                cmd.game_cmd.count = 1;
+                            }
+                            else {
+                                cmd.game_cmd.count = stoi(str);
+                            }
+                        }
+                    }
+                    else if (str.substr(0, 4) == "home") {
+                        // move pileX homeX
+                        cmd.game_cmd.type = move_pile_home;
+                        cmd.game_cmd.to = stoi(str.substr(4));
+
+                        if (cmd.game_cmd.to < 1 || cmd.game_cmd.to > NUM_OF_HOMES)
+                            cmd.type = invalid;
+                    }
+                    else {
+                        cmd.type = invalid;
+                    }
+                }
+                else {
+                    cmd.type = invalid;
+                }
+            }
+            catch (out_of_range& e) {
+                cmd.type = invalid;
+            }
+            catch (invalid_argument& e) {
+                cmd.type = invalid;
+            }
         }
+        //turn deck           turn 1 card from the deck
+        //turn pileX          turn 1 card from pile (1-7)
         else if (str == "turn" || str == "t") {
             cmd.type = game_cmd;
             cmd.game_cmd.type = turn_deck;
